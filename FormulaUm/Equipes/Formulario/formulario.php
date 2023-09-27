39db1c40df1039f7fc17cb8307b1b5dc31f81daa
Gabriel Anjos <gabriel.anjos@businessintegrator.com.br>
2023-09-27 19:28:50 -0300

    Ajustando formulario

---
+++ FormulaUm/Equipes/Formulario/formulario.php
@@ -0,0 +1,27 @@
+<?php
+// Verifica se o formulário foi enviado
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Obtém os dados do formulário
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $country = $_POST['country'];
+  $team = $_POST['team'];
+  $experience = $_POST['experience'];
+  
+  // Realiza as ações desejadas com os dados
+  // Por exemplo, você pode armazenar os dados em um banco de dados ou enviar por email
+  
+  // Exemplo de armazenamento em um arquivo de texto
+  $dados = "Nome: $name\nEmail: $email\nPaís: $country\nEquipe favorita: $team\nExperiência: $experience\n";
+  file_put_contents('dados_formulario.txt', $dados, FILE_APPEND);
+  
+  // Exemplo de envio por email
+  $assunto = "Novo cadastro - Fórmula 1";
+  $mensagem = "Um novo cadastro foi recebido:\n\nNome: $name\nEmail: $email\nPaís: $country\nEquipe favorita: $team\nExperiência: $experience\n";
+  mail('seu-email@example.com', $assunto, $mensagem);
+  
+  // Redireciona para uma página de confirmação
+  header('Location: confirmacao.html');
+  exit;
+}
+?>
